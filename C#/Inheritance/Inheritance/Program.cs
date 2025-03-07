bcecf019ff68e6d868db1fd18a3e5de1e743f1b9
Anilkumar-B <anilkumarb1004@gmail.com>
2025-03-07 12:01:24 +0530

    Add files via upload

---
+++ C#/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car { Brand = "Toyota", Speed = 120, NumberOfDoors = 4 };
+            Bike bike = new Bike { Brand = "Yamaha", Speed = 80, HasCarrier = true };
+
+            car.DisplayInfo();
+            Console.WriteLine($"Number of Doors: {car.NumberOfDoors}\n");
+
+            bike.DisplayInfo();
+            Console.WriteLine($"Has Carrier: {bike.HasCarrier}");
+        }
+    }
+}
---
+++ C#/Inheritance/Inheritance/Program1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Inheritance
+{
+    // Base class
+    class Vehicle
+    {
+        public string Brand { get; set; }
+        public int Speed { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Speed: {Speed} km/h");
+        }
+    }
+
+    // Derived class Car
+    class Car : Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+    }
+
+    // Derived class Bike
+    class Bike : Vehicle
+    {
+        public bool HasCarrier { get; set; }
+    }
+
+}
+
+
+
+    
