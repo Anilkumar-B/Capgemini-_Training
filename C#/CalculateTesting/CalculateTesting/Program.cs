d895fcaab92e6c9bce8653aae6176ee6dbcd69b1
Anilkumar-B <anilkumarb1004@gmail.com>
2025-03-07 11:32:44 +0530

    Add files via upload

---
+++ C#/CalculateTesting/CalculateTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnitLite;
+
+namespace NUnit_Test_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new AutoRun().Execute(args);
+        }
+    }
+}
